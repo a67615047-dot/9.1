6ccb4e9b04de0f12b1953eeedcf8dd6ea14a33a6
a67615047-dot <a67615047@gmail.com>
2026-09-22 13:35:25 +0300

    perimetrs.py ir izveidota

--- perimetrs.py
+++ perimetrs.py
@@ -1,1 +1,4 @@
-
+garums = float(input("Ievadi zemesgabala garumu metros: "))
+platums = float(input("Ievadi zemesgabala platumu metros: "))
+perimetrs = 2*(garums+platums)
+print("Nepieciešamais apžogojuma garums ir", perimetrs, "m")
